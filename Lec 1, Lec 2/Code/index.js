d343bb5be681717deb9efd88218ce36dd1718a65
Ayush Shrivastav <ayushshrivastav575@gmail.com>
2024-01-05 02:52:41 +0530

    Adding .env

--- Lec 1, Lec 2/Code/index.js
+++ Lec 1, Lec 2/Code/index.js
@@ -1,21 +1,20 @@
-console.log("Chai ur Code");
+require('dotenv').config()
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 4000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 app.get("/login",(req,res)=>{
-    res.send("<h1>Successful Login, Ayush Shrivastav</h1>")
+    res.send("<h1>Successful Login, Welcome Ayush Shrivastav</h1>")
 })
 
 app.get("/chai",(req,res)=>{
     res.send("<h2>Chai with Code</h2>")
 })
 
-
-app.listen(port, () => {
+app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${port}`)
 })
